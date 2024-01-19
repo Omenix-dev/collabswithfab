e3ddc51c5b6cd230e5c02ad2072f7ea14b9c6d11
GABRIEL EGHOSA EMMANUEL <43476099+Virifortissimi@users.noreply.github.com>
2024-01-19 16:42:04 +0100

    Update

--- MedicalRecordsData/Entities/MedicalRecordsEntity/Patient.cs
+++ MedicalRecordsData/Entities/MedicalRecordsEntity/Patient.cs
@@ -22,17 +22,20 @@ namespace MedicalRecordsData.Entities.MedicalRecordsEntity
 		public DateTime DateOfBirth { get; set; }
 		public string Email { get; set; }
 		public string PhoneNumber { get; set; }
-		public string UserName { get; set; }
-		public string Password { get; set; }
-		public string Nationality { get; set; }
 		public string StateOfOrigin { get; set; }
 		public string LGA { get; set; }
 		public string PlaceOfBirth { get; set; }
 		public string MaritalStatus { get; set; }
 
+		//PatientAuth
+		public int UserId { get; set; }
+
 		//Staff taking care of patient
 		public int NurseId { get; set; }
 		public int DoctorId { get; set; }
+
+		//Clinic
+		public int ClinicId { get; set; }
 
 
 		//Navigation Properties
